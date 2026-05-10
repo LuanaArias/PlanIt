16067510601c4ee37f4a69564a941e5c182c73ed
Luana <luanaariaaas@gmail.com>
2026-05-10 17:10:41 -0300

    arreglando estilos

--- frontend/src/components/Charts/TasksBarChart.jsx
+++ frontend/src/components/Charts/TasksBarChart.jsx
@@ -10,7 +10,7 @@ import {
 export function TasksBarChart({ tasks }) {
     const grouped = {};
     tasks.forEach(task => {
-        if(task.completedAt){
+        if (task.completedAt) {
             const date =
                 new Date(task.completedAt)
                     .toLocaleDateString();
@@ -27,23 +27,42 @@ export function TasksBarChart({ tasks }) {
     );
 
     return (
-
         <div className="chart-card">
             <h2>
                 Tareas completadas
             </h2>
             {data.length > 0 ? (
-                <ResponsiveContainer width="100%" height={300}>
+                <ResponsiveContainer
+                    width="100%"
+                    height={240}
+                >
                     <BarChart data={data}>
-                        <XAxis dataKey="date" />
-                        <YAxis />
+                        <XAxis
+                            dataKey="date"
+                            tick={{ fill: "#DAD4DF" }}
+                            axisLine={false}
+                            tickLine={false}
+                        />
+                        <YAxis
+                            tick={{ fill: "#DAD4DF" }}
+                            allowDecimals={false}
+                            axisLine={false}
+                            tickLine={false}
+                            domain={[0, "auto"]}
+                        />
                         <Tooltip />
-                        <Bar dataKey="total" />
+                        <Bar
+                            dataKey="total"
+                            fill="#D7C5D6"
+                            radius={[12, 12, 0, 0]}
+                        />
+
                     </BarChart>
                 </ResponsiveContainer>
             ) : (
                 <p>No hay tareas completadas todavía</p>
             )}
+
         </div>
     );
 }
